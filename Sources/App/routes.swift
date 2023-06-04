fdbc78d4e2afe0265db8430d09f043e95220810e
bhishaksanyal <sanyalbishak93@gmail.com>
2023-06-04 20:36:10 +0530

    chore: strongly typed route parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,11 +13,25 @@ func routes(_ app: Application) throws {
         "Movie Route"
     }
     
+    // Strongly typed route parameters
+    app.get("customer", ":customerID") { req async throws -> String in
+        
+        guard let customerID = req.parameters.get("customerID", as: Int.self) else {
+            throw Abort(.unsupportedMediaType)
+        }
+        
+        return "Requesting for customer data with ID: \(customerID)"
+    }
+    
     // movies/action
     // movies/kids
     // movies/horror
-    app.get("movies", ":genre") { req async -> String in
-        let genre = req.parameters.get("genre")
-        return "All movies under genre \(String(describing: genre!))"
+    // route parameters
+    app.get("movies", ":genre") { req async throws -> String in
+        
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies under genre: \(String(describing: genre))"
     }
 }
