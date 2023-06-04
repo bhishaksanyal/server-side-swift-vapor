adbda83560e2632da7918bf02e1dcba8f6f19058
bhishaksanyal <sanyalbishak93@gmail.com>
2023-06-04 20:02:41 +0530

    chore: dynamic request params

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,16 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("movies") { req async -> String in
+        "Movie Route"
+    }
+    
+    // movies/action
+    // movies/kids
+    // movies/horror
+    app.get("movies", ":genre") { req async -> String in
+        let genre = req.parameters.get("genre")
+        return "All movies under genre \(String(describing: genre!))"
+    }
 }
