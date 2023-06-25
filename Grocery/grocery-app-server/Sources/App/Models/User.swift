08050f441c5fa9ebdc68cf158f53b47835972ccc
bhishaksanyal <sanyalbishak93@gmail.com>
2023-06-25 19:22:53 +0530

    feat: added user model

---
+++ Grocery/grocery-app-server/Sources/App/Models/User.swift
@@ -0,0 +1,45 @@
+//
+//  User.swift
+//  
+//
+//  Created by Bhishak Sanyal on 25/06/23.
+//
+
+import Foundation
+import Fluent
+import Vapor
+
+final class User: Model, Content, Validatable {
+    
+    static let schema = "users"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "username")
+    var username: String
+    
+    @Field(key: "password")
+    var password: String
+    
+    @Field(key: "createdOn")
+    var createdOn: String
+    
+    init() {  }
+    
+    init(id: UUID? = nil, username: String, password: String, createdOn: String) {
+        self.id = id
+        self.username = username
+        self.password = password
+        self.createdOn = createdOn
+    }
+    
+    static func validations(_ validations: inout Validations) {
+        validations.add("username", as: String.self, is: !.empty, customFailureDescription: "Username should not be empty")
+        validations.add("password", as: String.self, is: !.empty, customFailureDescription: "Password should not be empty")
+        validations.add("createdOn", as: String.self, is: !.empty, customFailureDescription: "Timestamp should not be empty")
+        
+        validations.add("password", as: String.self, is: .count(6...10), customFailureDescription: "Password should be 6 to 10 character long")
+    }
+    
+}
